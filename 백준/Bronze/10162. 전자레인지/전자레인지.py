839c8c3600a0b15c5829c6fcded26d011dd25b81
Kimminsong1126 <63196792+Kimminsong1126@users.noreply.github.com>
2023-09-05 11:28:19 +0900

    [Bronze III] Title: 전자레인지, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10162. 전자레인지/전자레인지.py
+++ 백준/Bronze/10162. 전자레인지/전자레인지.py
@@ -1,19 +1,8 @@
-t = int(input())
-a=0
-b=0
-c=0
+n=int(input())
 
-if t % 10 != 0:
+if n%10!=0:
     print(-1)
 else:
-    while (t != 0):
-        if t >= 300:
-            a = t // 300
-            t = t % 300
-        elif t < 300 and t >= 60:
-            b = t // 60
-            t = t % 60
-        elif t<60 and t>=10:
-            c = t // 10
-            t = t % 10
-    print(a, b, c)
+    for i in [300,60,10]:
+        print(n//i, end=" ")
+        n=n%i
